2aac5d6acaa020640d64511f07faacd68fa6811d
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2018-11-15 11:01:07 +0100

    Bump version: 0.1.1-dev13 → 1.0.0

--- mlbench_core/__init__.py
+++ mlbench_core/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for mlbench_core."""
 
-__version__ = '0.1.1-dev13'
+__version__ = '1.0.0'
 
 from . import api
 from . import controlflow
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mlbench/mlbench_core',
-    version='0.1.1-dev13',
+    version='1.0.0',
     zip_safe=False,
 )
