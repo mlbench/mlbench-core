909696058c1e7d67a253ce483172c5b736585fcc
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-01-31 12:07:19 +0100

    Version 1.2.1 release

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mlbench/mlbench_core',
-    version='1.2.0',
+    version='1.2.1',
     zip_safe=False,
 )
