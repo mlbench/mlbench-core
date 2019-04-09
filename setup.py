2b6faf033c5f35a5a938848330bcd3060fe9ff9d
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-04-09 11:00:51 +0200

    Dev release 1.3.4-dev2 for testing

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mlbench/mlbench_core',
-    version='1.3.4',
+    version='1.3.4-dev2',
     zip_safe=False,
 )
