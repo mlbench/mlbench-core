88f94ae9b31cf23ce2e353b79c06d71e0191ec35
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-05-02 15:05:04 +0200

    Fixes unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -30,21 +30,21 @@ def test_tracker_goal(mocker):
 
     tracker.train()
 
-    tracker.record_metric(metric, 0.7, log_to_api=True)
+    tracker.record_metric(metric, 69, log_to_api=True)
 
     assert not tracker.goal_reached
 
-    tracker.record_metric(metric, 0.95, log_to_api=True)
+    tracker.record_metric(metric, 70, log_to_api=True)
 
     assert not tracker.goal_reached
 
     tracker.validation()
 
-    tracker.record_metric(metric, 0.7, log_to_api=True)
+    tracker.record_metric(metric, 69, log_to_api=True)
 
     assert not tracker.goal_reached
 
-    tracker.record_metric(metric, 0.95, log_to_api=True)
+    tracker.record_metric(metric, 70, log_to_api=True)
 
     assert tracker.goal_reached
     assert patched.log.call_count == 5
