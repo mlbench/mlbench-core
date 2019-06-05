e4c12b18c58b9bf87c7d564e194a704855b1fe9e
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-06-05 10:58:20 +0200

    Changes unit test for new tracker implementation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -30,22 +30,22 @@ def test_tracker_goal(mocker):
 
     tracker.train()
 
-    tracker.record_metric(metric, 69, log_to_api=True)
+    tracker.record_stat('global_Prec@1', 69, log_to_api=True)
 
     assert not tracker.goal_reached
 
-    tracker.record_metric(metric, 70, log_to_api=True)
+    tracker.record_stat('global_Prec@1', 70, log_to_api=True)
 
     assert not tracker.goal_reached
 
     tracker.validation()
 
-    tracker.record_metric(metric, 69, log_to_api=True)
+    tracker.record_stat('global_Prec@1', 69, log_to_api=True)
 
     assert not tracker.goal_reached
 
-    tracker.record_metric(metric, 70, log_to_api=True)
+    tracker.record_stat('global_Prec@1', 70, log_to_api=True)
 
     assert tracker.goal_reached
-    assert patched.log.call_count == 5
-    assert patched.log.call_args[0][3] == "TaskResult"
+    assert patched.log.call_count == 6
+    assert patched.log.call_args[0][3] == "TotalCumulativeTrainTime"
