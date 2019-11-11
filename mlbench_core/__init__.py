3598e576b7a2a3b17a94dc71392799e9c423cab8
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-11-11 14:14:34 +0100

    Version 2.1.1

--- mlbench_core/__init__.py
+++ mlbench_core/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for mlbench_core."""
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 
 from . import api
 from . import controlflow
--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mlbench/mlbench_core',
-    version='2.1.0',
+    version='2.1.1',
     zip_safe=False,
 )
