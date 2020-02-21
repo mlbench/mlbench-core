56a130bc76e3c8e9bd5109c63d1a0aa43b3fc0c6
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-02-21 17:23:07 +0100

    Refactor code for PEP8 compliance lr_scheduler/

--- mlbench_core/lr_scheduler/pytorch/lr.py
+++ mlbench_core/lr_scheduler/pytorch/lr.py
@@ -1,30 +1,33 @@
 # -*- coding: utf-8 -*-
 
+import math
 from bisect import bisect_right
+
 import numpy as np
 from torch.optim.lr_scheduler import LambdaLR
-import math
 
 
 def const(optimizer):
     return LambdaLR(optimizer, lr_lambda=lambda x: 1.0)
 
 
-def triangular_learning_rates(optimizer, base_lr, max_lr, cycle_length, scale_fn, extra, mode):
+def triangular_learning_rates(optimizer, base_lr, max_lr, cycle_length,
+                              scale_fn, extra, mode):
     """ Linearily Scale Learning Rate
 
-    If one cycle is applied with length smaller than the total number of iterations, then
-    use small learning rate for the remaining iterations.
+    If one cycle is applied with length smaller than the total number of
+    iterations, then use small learning rate for the remaining iterations.
 
     Args:
-        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given model.
+        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given
+            model.
         base_lr (float): Lower bound and initial learning rate in a cycle.
         max_lr (float): Upper bound in a cycle
         cycle_length (int): Length of a cycle in terms of batches.
         scale_fn(:func:`Function`): The scaling function.
         extra (int): The number of extra epochs to perform after a cycle
-        mode (str): The scaling mode to use. One of `linear`, `triangular`, `one_cycle`,
-            `triangular2` or `exp_range`
+        mode (str): The scaling mode to use. One of `linear`, `triangular`,
+            `one_cycle`, `triangular2` or `exp_range`
 
     Returns:
         A learning rate scheduler (:obj:`torch.optim.lr_scheduler.LambdaLR`)
@@ -36,8 +39,8 @@ def triangular_learning_rates(optimizer, base_lr, max_lr, cycle_length, scale_fn
             if iterations <= cycle_length:
                 cycle = np.floor(1 + iterations / (2 * step_size))
                 x = np.abs(iterations / step_size - 2 * cycle + 1)
-                lr = base_lr + (max_lr - base_lr) * np.maximum(0,
-                                                               (1 - x)) * scale_fn(cycle, iterations)
+                lr = base_lr + (max_lr - base_lr) * np.maximum(0, (1 - x)) * \
+                     scale_fn(cycle, iterations)  # noqa
             else:
                 lr = base_lr * extra
             return lr / base_lr
@@ -46,7 +49,8 @@ def triangular_learning_rates(optimizer, base_lr, max_lr, cycle_length, scale_fn
             cycle = np.floor(1 + iterations / (2 * step_size))
             x = np.abs(iterations / step_size - 2 * cycle + 1)
             lr = base_lr + (max_lr - base_lr) * np.maximum(0,
-                                                           (1 - x)) * scale_fn(cycle, iterations)
+                                                           (1 - x)) * scale_fn(
+                cycle, iterations)
             return lr / base_lr
 
     # Use base_lr to overwrite the --lr
@@ -56,14 +60,16 @@ def triangular_learning_rates(optimizer, base_lr, max_lr, cycle_length, scale_fn
     return LambdaLR(optimizer, lr_lambda=f)
 
 
-def cyclical_learning_rates(optimizer, mode, gamma, cycle_length, base_lr, max_lr, extra_epochs):
+def cyclical_learning_rates(optimizer, mode, gamma, cycle_length, base_lr,
+                            max_lr, extra_epochs):
     """ Cyclically Scale Learning Rate
 
-    If one cycle is applied with length smaller than the total number of iterations, then
-    use small learning rate for the remaining iterations.
+    If one cycle is applied with length smaller than the total number of
+    iterations, then use small learning rate for the remaining iterations.
 
-    Since :cite:`smith2017cyclical` mentioned that triangular, Welch, Hann windows produce equivalent results,
-    we only implement triangular learning rate policy, also known as **linear cycle**.
+    Since :cite:`smith2017cyclical` mentioned that triangular, Welch,
+    Hann windows produce equivalent results, we only implement triangular
+    learning rate policy, also known as **linear cycle**.
 
     The original implementation of :cite:`smith2017cyclical` can be found from
     `here <https://github.com/bckenstler/CLR>`_.
@@ -71,9 +77,10 @@ def cyclical_learning_rates(optimizer, mode, gamma, cycle_length, base_lr, max_l
     :cite:`smith2017super` uses one cycle with extra epochs.
 
     Args:
-        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given model.
-        mode (str): The scaling mode to use. One of `linear`, `triangular`, `one_cycle`,
-            `triangular2` or `exp_range`
+        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given
+            model.
+        mode (str): The scaling mode to use. One of `linear`, `triangular`,
+            `one_cycle`, `triangular2` or `exp_range`
         base_lr (float): Lower bound and initial learning rate in a cycle.
         max_lr (float): Upper bound in a cycle
         max_lr (float): The maximum learning rate
@@ -83,16 +90,20 @@ def cyclical_learning_rates(optimizer, mode, gamma, cycle_length, base_lr, max_l
         A learning rate scheduler (:obj:`torch.optim.lr_scheduler.LambdaLR`)
     """
     if mode in ['linear', 'triangular', 'one_cycle']:
-        def scale_fn(cycle, iterations): return 1.
+        def scale_fn(cycle, iterations):
+            return 1.
     elif mode == 'triangular2':
-        def scale_fn(cycle, iterations): return 1 / (2. ** (cycle - 1))
+        def scale_fn(cycle, iterations):
+            return 1 / (2. ** (cycle - 1))
     elif mode == 'exp_range':
-        def scale_fn(cycle, iterations): return gamma ** iterations
+        def scale_fn(cycle, iterations):
+            return gamma ** iterations
     else:
         raise ValueError("Cycle mode {} not support.".format(mode))
 
     return triangular_learning_rates(optimizer, base_lr, max_lr,
-                                     cycle_length=cycle_length, scale_fn=scale_fn,
+                                     cycle_length=cycle_length,
+                                     scale_fn=scale_fn,
                                      extra=extra_epochs,
                                      mode=mode)
 
@@ -107,29 +118,35 @@ def multistep_learning_rates_with_warmup(optimizer,
                                          warmup_linear_scaling=False):
     """ Multistep Learning Rate Schedule with warmup
 
-    In :cite:`goyal2017accurate`, warmup is used in order to apply the ``Linear Scaling Rule``.
-    Starting from the ``base_lr``, lr gradually increases to ``base_lr * scaling_factor``.
-    Then use multiply the learning rate by ``gamma`` at specified milestones.
+    In :cite:`goyal2017accurate`, warmup is used in order to apply the
+    ``Linear Scaling Rule``. Starting from the ``base_lr``, lr gradually
+    increases to ``base_lr * scaling_factor``. Then use multiply the learning
+    rate by ``gamma`` at specified milestones.
     See :cite:`ginsburg2018large`
 
     Args:
-        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given model.
+        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the
+            given model.
         world_size (int): The total number of workers
         lr (float): The initial learning rate
         gamma (float): Decay factor for learning rate
-        milestones (:obj:`list` of :obj:`int`): The epochs/steps at which to reduce the
-            learning rate
-        warmup_duration (int): The number of epochs to perform warmup before regular
-            lr scaling starts. Default: `None`
-        warmup_lr (float): The learning rate to use for the warmup epochs. Default: `None`
-        warmup_linear_scaling (bool): Whether or not to linearily scale lr during
-            warmup. Default: `False`
+        milestones (:obj:`list` of :obj:`int`): The epochs/steps at which to
+            reduce the learning rate
+        warmup_duration (int): The number of epochs to perform warmup before
+            regular lr scaling starts.
+            Default: `None`
+        warmup_lr (float): The learning rate to use for the warmup epochs.
+            Default: `None`
+        warmup_linear_scaling (bool): Whether or not to linearily scale lr
+            during warmup.
+            Default: `False`
     Returns:
         A learning rate scheduler (:obj:`torch.optim.lr_scheduler.LambdaLR`)
     """
     if bool(warmup_duration) != bool(warmup_lr):
         raise ValueError(
-            "Either both or none of warmup_duration and warmup_lr have to be set")
+            "Either both or none of warmup_duration and warmup_"
+            "lr have to be set")
 
     scaling_factor = 1
 
@@ -147,14 +164,15 @@ def multistep_learning_rates_with_warmup(optimizer,
                          'Got {}'.format(milestones))
 
     if warmup_duration >= milestones[0]:
-        raise ValueError("The scaling phase should be earlier than the first milestone."
-                         "Got {} and {}".format(warmup_duration, milestones[0]))
+        raise ValueError(
+            "The scaling phase should be earlier than the first milestone."
+            "Got {} and {}".format(warmup_duration, milestones[0]))
 
     def f(duration):
         if warmup_lr and duration <= warmup_duration:
             warmup_progress = duration / warmup_duration
             lr = warmup_progress * base_lr + \
-                (1 - warmup_progress) * warmup_init_lr
+                 (1 - warmup_progress) * warmup_init_lr  # noqa
         else:
             lr = base_lr * gamma ** bisect_right(milestones, duration)
         return lr / base_lr
@@ -169,20 +187,26 @@ class MultistepLearningRatesWithWarmup(LambdaLR):
     """ Multistep Learningrate Scheduler with Warmup Period
 
     Args:
-        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given model.
+        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the
+            given model.
         world_size (int): The total number of workers
         gamma (float): Decay factor for learning rate
-        milestones (:obj:`list` of :obj:`int`): The epochs/steps at which to reduce the
-            learning rate
+        milestones (:obj:`list` of :obj:`int`): The epochs/steps at which to
+            reduce the learning rate
         lr (float): The initial learning rate
-        warmup_duration (int): The number of epochs to perform warmup before regular
-            lr scaling starts. Default: `None`
-        warmup_linear_scaling (bool): Whether or not to linearily scale lr during
-            warmup. Default: `False`
-        warmup_init_lr (float): The initial learning rate to use for the warmup epochs. Default: `None`
+        warmup_duration (int): The number of epochs to perform warmup
+            before regular lr scaling starts.
+            Default: `None`
+        warmup_linear_scaling (bool): Whether or not to linearily scale lr
+            during warmup.
+            Default: `False`
+        warmup_init_lr (float): The initial learning rate to use for the
+            warmup epochs.
+            Default: `None`
     Returns:
         A learning rate scheduler (:obj:`torch.optim.lr_scheduler.LambdaLR`)
     """
+
     def __init__(self,
                  optimizer,
                  world_size,
@@ -193,12 +217,14 @@ class MultistepLearningRatesWithWarmup(LambdaLR):
                  warmup_linear_scaling=True,
                  warmup_init_lr=None):
         if list(milestones) != sorted(milestones):
-            raise ValueError('Milestones should be a list of increasing integers.'
-                             'Got {}'.format(milestones))
+            raise ValueError(
+                'Milestones should be a list of increasing integers.'
+                'Got {}'.format(milestones))
 
         if warmup_duration >= milestones[0]:
-            raise ValueError("The scaling phase should be earlier than the first milestone."
-                             "Got {} and {}".format(warmup_duration, milestones[0]))
+            raise ValueError(
+                "The scaling phase should be earlier than the first milestone."
+                "Got {} and {}".format(warmup_duration, milestones[0]))
 
         self.optimizer = optimizer
         self.gamma = gamma
@@ -222,21 +248,26 @@ class MultistepLearningRatesWithWarmup(LambdaLR):
         if duration <= self.warmup_duration:
             progress = duration / self.warmup_duration
             lr = progress * self.warmup_scaled_lr + \
-                (1 - progress) * self.warmup_init_lr
+                 (1 - progress) * self.warmup_init_lr  # noqa
         else:
             lr = self.warmup_scaled_lr * self.gamma ** \
-                bisect_right(self.milestones, duration)
+                 bisect_right(self.milestones, duration)  # noqa
         return lr / self.base_lr
 
 
 class SparsifiedSGDLR(LambdaLR):
-    """ Learning rate schedule for sparsifiedSGD (gamma / l2_coef * (t + shifting_param))
+    """ Learning rate schedule for sparsifiedSGD
+    (gamma / l2_coef * (t + shifting_param))
 
     Args:
-        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given model.
-        gamma (float): The constant value in the numerator of the learning rate schedule formula
-        l2_coef (float): The regularization rate which is used in the denominator of the learning rate schedule formula
-        shifting_param (float): The constant value in the denominator of the learning rate schedule formula
+        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the
+            given model.
+        gamma (float): The constant value in the numerator of the
+            learning rate schedule formula
+        l2_coef (float): The regularization rate which is used in the
+            denominator of the learning rate schedule formula
+        shifting_param (float): The constant value in the denominator of the
+            learning rate schedule formula
     """
 
     def __init__(self, optimizer, gamma, l2_coef, shifting_param):
@@ -260,9 +291,12 @@ class TimeDecayLR(LambdaLR):
     Time based decay learning rate schedule for SGD (alpha / (t + beta))
 
     Args:
-        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given model.
-        alpha (float): The constant value in the numerator of the learning rate schedule formula
-        beta (float): The constant value in the denominator of the learning rate schedule formula
+        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the
+            given model.
+        alpha (float): The constant value in the numerator of the
+            learning rate schedule formula
+        beta (float): The constant value in the denominator of the
+            learning rate schedule formula
     Returns:
         A learning rate scheduler (:obj:`torch.optim.lr_scheduler.LambdaLR`)
     """
@@ -289,13 +323,15 @@ class SQRTTimeDecayLR(LambdaLR):
     Time based decay learning rate schedule for SGD (alpha / sqrt(t))
 
     Args:
-        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the given model.
-        alpha (float): The constant value in the numerator of the learning rate schedule formula
+        optimizer (:obj:`torch.optim.Optimizer`): an optimizer for the
+            given model.
+        alpha (float): The constant value in the numerator of the
+            learning rate schedule formula
     Returns:
         A learning rate scheduler (:obj:`torch.optim.lr_scheduler.LambdaLR`)
     """
-    def __init__(self, optimizer, alpha):
 
+    def __init__(self, optimizer, alpha):
         self.alpha = alpha
         self.optimizer = optimizer
 
--- mlbench_core/lr_scheduler/tensorflow/lr.py
+++ mlbench_core/lr_scheduler/tensorflow/lr.py
@@ -2,7 +2,8 @@ r"""Learning rate scheduling in tensorflow.
 
 The manual_stepping function is taken from :
 
-https://github.com/tensorflow/models/blob/master/research/object_detection/utils/learning_schedules.py
+https://github.com/tensorflow/models/blob/master/research/object_detection
+/utils/learning_schedules.py
 """
 
 import tensorflow as tf
@@ -13,21 +14,25 @@ def manual_stepping(global_step, boundaries, rates, warmup=False):
 
     This function provides fine grained control over learning rates.  One must
     specify a sequence of learning rates as well as a set of integer steps
-    at which the current learning rate must transition to the next.  For example,
-    if boundaries = [5, 10] and rates = [.1, .01, .001], then the learning
-    rate returned by this function is .1 for global_step=0,...,4, .01 for
-    global_step=5...9, and .001 for global_step=10 and onward.
+    at which the current learning rate must transition to the next.
+    For example, if boundaries = [5, 10] and rates = [.1, .01, .001],
+    then the learning rate returned by this function is .1
+    for global_step=0,...,4, .01 for global_step=5...9, and .001
+    for global_step=10 and onward.
 
     Args:
-        global_step (:obj:`tf.Tensor`): int64 (scalar) tensor representing global step.
+        global_step (:obj:`tf.Tensor`): int64 (scalar) tensor representing
+            global step.
         boundaries (list): a list of global steps at which to switch learning
-        rates (list): a list of (float) learning rates corresponding to intervals between
-            the boundaries.  The length of this list must be exactly len(boundaries) + 1.
-        warmup (bool, optional): Defaults to False. Whether to linearly interpolate learning
-            rate for steps in [0, boundaries[0]].
+        rates (list): a list of (float) learning rates corresponding to
+            intervals between the boundaries.
+            The length of this list must be exactly len(boundaries) + 1.
+        warmup (bool, optional): Defaults to False. Whether to linearly
+            interpolate learning rate for steps in [0, boundaries[0]].
 
     Raises:
-        ValueError: boundaries is a strictly increasing list of positive integers
+        ValueError: boundaries is a strictly increasing list of positive
+            integers
         ValueError: len(rates) == len(boundaries) + 1
         ValueError: boundaries[0] != 0
 
@@ -58,8 +63,9 @@ def manual_stepping(global_step, boundaries, rates, warmup=False):
     else:
         boundaries = [0] + boundaries
     num_boundaries = len(boundaries)
-    rate_index = tf.reduce_max(tf.where(tf.greater_equal(global_step, boundaries),
-                                        list(range(num_boundaries)),
-                                        [0] * num_boundaries))
+    rate_index = tf.reduce_max(
+        tf.where(tf.greater_equal(global_step, boundaries),
+                 list(range(num_boundaries)),
+                 [0] * num_boundaries))
     return tf.reduce_sum(rates * tf.one_hot(rate_index, depth=num_boundaries),
                          name='learning_rate')
