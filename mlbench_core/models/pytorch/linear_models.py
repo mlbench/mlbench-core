052a320176c4de79f5c808c91c364c0bd0b5f75a
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-02-26 14:05:04 +0100

    Add tests for linear models

--- mlbench_core/models/pytorch/linear_models.py
+++ mlbench_core/models/pytorch/linear_models.py
@@ -1,8 +1,4 @@
 import torch
-import math
-import torch.nn.functional as F
-from torch.nn.parameter import Parameter
-import numpy as np
 
 
 class LogisticRegression(torch.nn.Module):
@@ -32,9 +28,8 @@ class LinearRegression(torch.nn.Module):
     """
 
     def __init__(self, n_features):
-        super(RidgeRegression, self).__init__()
+        super(LinearRegression, self).__init__()
         self.linear = torch.nn.Linear(n_features, 1, bias=False)
 
     def forward(self, x):
         return self.linear(x)
-
