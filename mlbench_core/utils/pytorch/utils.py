1b5c8bbf8ba2d19e8b7add18b51dabdd1c0f03c6
Martin Milenkoski <martinmilenkoski53@gmail.com>
2020-04-26 15:29:21 +0200

    Add eps to orthogonalize

--- mlbench_core/utils/pytorch/utils.py
+++ mlbench_core/utils/pytorch/utils.py
@@ -2,13 +2,18 @@ import torch
 
 
 @torch.jit.script
-def orthogonalize(matrix):
-    """Function used to orthogonalize a matrix."""
+def orthogonalize(matrix, eps=torch.FloatTensor([1e-16])):
+    """Function used to orthogonalize a matrix.
+
+    Args:
+        matrix (torch.Tensor): Matrix to orthogonalize
+        eps (torch.FloatTensor): Used to avoid division by zero (default: 1e-16)
+    """
     n, m = matrix.shape
     for i in range(m):
         # Normalize the i'th column
         col = matrix[:, i : i + 1]
-        col /= torch.sqrt(torch.sum(col ** 2))
+        col /= torch.sqrt(torch.sum(col ** 2)) + eps
         # Project it on the rest and remove it
         if i + 1 < m:
             rest = matrix[:, i + 1 :]
