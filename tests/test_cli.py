7c3845a4b6abae2f21ecc750ddd38c073ce5eefd
giorgio <giorgiosaza@gmail.com>
2020-05-08 17:41:28 +0200

    comments

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -27,9 +27,16 @@ CREATE_GCLOUD_DEFAULTS = {
 
 
 def get_gcloud_cmd_line(args, option_dict=None):
-    """
-    args: (list) command-line arguments
-    option_dict: (dict) key-value for options line in CREATE_GCLOUD_DEFAULTS
+    """Constructs command line for gcloud commands
+
+    Returns command line as a list, e.g. ["3", "myrelease", "-g", "1", "--gpu-type", "nvidia-tesla-k80"].
+
+    Args: 
+        args (list): command-line arguments (e.g. [3, "myrelease"])
+        option_dict (dict): command-line options, as in CREATE_GCLOUD_DEFAULTS
+
+    Returns:
+        list: command line constructed from inputs
     """
 
     flags = {
@@ -68,6 +75,18 @@ def get_gcloud_cmd_line(args, option_dict=None):
 
 
 def status_mock_helper(mocker, gcloud_mock, run_name):
+    """Generates patched objects for "mlbench status" command.
+
+    This is a helper function used in fixtures that test "mlbench status".
+
+    Args:
+        mocker (pytest.fixture)
+        gcloud_mock (pytest.fixture)
+        run_name (string)
+
+    Returns:
+        tuple: patched ApiClient, run ID and run name
+    """
     setup = mocker.patch("mlbench_core.cli.cli.setup_client_from_config")
     setup.return_value = True
 
@@ -87,6 +106,18 @@ def status_mock_helper(mocker, gcloud_mock, run_name):
 
 
 def create_gcloud_test_helper(mocker, gcloud_mock, args, option_dict=None):
+    """Tests creation of gcloud cluster
+
+    Tests that the correct arguments are passed to Gcloud, Tiller, etc... based 
+    on the input arguments and options. 
+
+    Args:
+        args (list): command-line arguments
+        option_dict (dict): command-line options, as in CREATE_GCLOUD_DEFAULTS. Any 
+                            missing options are substituted with the defaults.
+    """
+
+
     container_v1 = gcloud_mock["containerv1"]
     tiller = gcloud_mock["tiller"]
     auth = gcloud_mock["auth"]
@@ -152,6 +183,8 @@ def create_gcloud_test_helper(mocker, gcloud_mock, args, option_dict=None):
 
 @pytest.fixture
 def gcloud_auth(mocker):
+    """Patches google.auth.default to bypass authentication"""
+    
     m = mocker.patch("google.auth.default")
     m.return_value = (m.credentials, "test_project")
 
@@ -161,6 +194,18 @@ def gcloud_auth(mocker):
 
 @pytest.fixture
 def gcloud_mock(mocker, gcloud_auth):
+    """Patches all gcloud objects and makes loops run once.
+
+    Loops like 
+
+        while response.status < response.DONE:
+            #do something 
+    
+    are executed once.
+
+    Returns:
+        dict: dictionary of patched objects
+    """
 
     # One response loop
     container_v1 = mocker.patch("google.cloud.container_v1")
@@ -203,11 +248,13 @@ def gcloud_mock(mocker, gcloud_auth):
 
 @pytest.fixture
 def status_mock(mocker, gcloud_mock):
+    """Patches status function when a run is found"""
     return status_mock_helper(mocker, gcloud_mock, "test-run-name")
 
 
 @pytest.fixture
 def status_mock_no_run(mocker, gcloud_mock):
+    """Patches status function when no runs are found"""
     return status_mock_helper(mocker, gcloud_mock, None)
 
 
@@ -215,6 +262,8 @@ def status_mock_no_run(mocker, gcloud_mock):
 
 
 def test_invalid_num_workers(mocker, gcloud_auth):
+    """Tests create-cluster with n_workers < 2"""
+
     res = runner.invoke(cli_group, ["create-cluster", "gcloud", "1", "test"])
     assert type(res.exception) == AssertionError
 
@@ -239,11 +288,17 @@ def test_invalid_num_workers(mocker, gcloud_auth):
         ([10, "test"], {"num_gpus": 3}),  # with gpu
     ],
 )
-def test_create_cluster(mocker, gcloud_mock, args, option_dict):
+def test_create_gcloud_cluster(mocker, gcloud_mock, args, option_dict):
+    """Tests creating a gcloud cluster.
+
+    Uses default values, non-default values and GPU (see decorator).
+    """
     create_gcloud_test_helper(mocker, gcloud_mock, args, option_dict)
 
 
-def test_delete_cluster(gcloud_mock):
+def test_delete_gcloud_cluster(gcloud_mock):
+    """Tests deleting a gcloud cluster"""
+
     gclient = gcloud_mock["gclient"]
     delete_cluster = gclient.delete_cluster
     get_operation = gclient.get_operation
@@ -267,6 +322,7 @@ def test_delete_cluster(gcloud_mock):
 
 
 def test_status(status_mock):
+    """Tests "mlbench status" command when a run is found"""
 
     client, rid, name = status_mock
     url = "my/test/url"
@@ -289,6 +345,7 @@ def test_status(status_mock):
 
 
 def test_status_no_run(status_mock_no_run):
+    """Tests "mlbench status" command when no run is found."""
 
     client, rid, _ = status_mock_no_run
     get_run_metrics = client.return_value.get_run_metrics
@@ -304,7 +361,8 @@ def test_status_no_run(status_mock_no_run):
 
 
 def test_delete_no_run(status_mock_no_run):
-    # Tests deleting a non-existent run
+    """Tests mlbench delete command when no run is found"""
+
     client, rid, _ = status_mock_no_run
     delete_run_client = client.return_value.delete_run
 
@@ -319,6 +377,7 @@ def test_delete_no_run(status_mock_no_run):
 
 
 def test_delete_run(status_mock):
+    """Tests mlbench delete command when a run is found"""
 
     client, rid, name = status_mock
     url = "my/test/url"
