a400629fd960e99dec471067f7e4b59253020b46
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-05-26 11:08:01 +0200

    Bump version and fix import

--- mlbench_core/dataset/nlp/pytorch/__init__.py
+++ mlbench_core/dataset/nlp/pytorch/__init__.py
@@ -1,5 +1,4 @@
 from .bpttwikitext2_dataset import BPTTWikiText2
-from .dataloader import Wikitext2
 from .wmt14_dataset import WMT14Dataset, build_collate_fn
 from .wmt17.batching import get_batches
 from .wmt17_dataset import WMT17Dataset
