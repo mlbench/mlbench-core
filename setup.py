90787fca1707b1fc8c5d53152457ed294f831403
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-06-02 09:47:41 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -78,6 +78,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras,
     url="https://github.com/mlbench/mlbench_core",
-    version="2.4.0-dev231",
+    version="2.4.0-dev233",
     zip_safe=False,
 )
