bd5004571b08ea000fadaa3cd28f4604ab1cd6ff
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-06-03 23:00:23 +0200

    Add numpy seed, manual_seed_all, and cudnn.deterministic

--- mlbench_core/__init__.py
+++ mlbench_core/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for mlbench_core."""
 
-__version__ = "2.4.0-dev233"
+__version__ = "2.4.0-dev237"
 
 from . import api, controlflow, dataset, evaluation, lr_scheduler, models, optim, utils
--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras,
     url="https://github.com/mlbench/mlbench_core",
-    version="2.4.0-dev233",
+    version="2.4.0-dev237",
     zip_safe=False,
 )
