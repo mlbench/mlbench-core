bfb2aa4e13a9c1adbcfda3c27c6a63cd70c97da2
giorgio <giorgiosaza@gmail.com>
2020-09-29 11:32:34 +0200

    black

--- mlbench_core/cli/cli.py
+++ mlbench_core/cli/cli.py
@@ -617,7 +617,7 @@ def delete_gcloud(name, zone, project):
     name_path = "projects/{}/locations/{}/".format(project, zone)
 
     cluster_path = "{}clusters/{}".format(name_path, name)
-    
+
     try:
         response = gclient.delete_cluster(None, None, None, name=cluster_path)
     except NotFound as e:
@@ -786,21 +786,15 @@ def create_gcloud(
             machine_type=machine_type,
             disk_size_gb=disk_size,
             preemptible=preemptible,
-            oauth_scopes=[
-                "https://www.googleapis.com/auth/devstorage.full_control",
-            ],
+            oauth_scopes=["https://www.googleapis.com/auth/devstorage.full_control",],
             **extraargs,
         ),
         addons_config=container_v1.types.AddonsConfig(
-            http_load_balancing=container_v1.types.HttpLoadBalancing(
-                disabled=True,
-            ),
+            http_load_balancing=container_v1.types.HttpLoadBalancing(disabled=True,),
             horizontal_pod_autoscaling=container_v1.types.HorizontalPodAutoscaling(
                 disabled=True,
             ),
-            kubernetes_dashboard=container_v1.types.KubernetesDashboard(
-                disabled=True,
-            ),
+            kubernetes_dashboard=container_v1.types.KubernetesDashboard(disabled=True,),
             network_policy_config=container_v1.types.NetworkPolicyConfig(
                 disabled=False,
             ),
@@ -1056,10 +1050,7 @@ def create_aws(
     # ensure that public IP addresses are assigned on launch in each subnet
     for subnet in subnets:
         ec2.modify_subnet_attribute(
-            MapPublicIpOnLaunch={
-                "Value": True,
-            },
-            SubnetId=subnet,
+            MapPublicIpOnLaunch={"Value": True,}, SubnetId=subnet,
         )
 
     # create a role and assign the policy needed for creating the EKS cluster
@@ -1335,11 +1326,7 @@ def create_aws(
             {
                 "FromPort": mlbench_port,
                 "IpProtocol": "tcp",
-                "IpRanges": [
-                    {
-                        "CidrIp": "0.0.0.0/0",
-                    },
-                ],
+                "IpRanges": [{"CidrIp": "0.0.0.0/0",},],
                 "ToPort": mlbench_port,
             },
         ],
@@ -1528,10 +1515,7 @@ def setup_aws_client_from_config(config):
         ],
         "contexts": [
             {
-                "context": {
-                    "cluster": cluster_arn,
-                    "user": cluster_arn,
-                },
+                "context": {"cluster": cluster_arn, "user": cluster_arn,},
                 "name": cluster_arn,
             }
         ],
