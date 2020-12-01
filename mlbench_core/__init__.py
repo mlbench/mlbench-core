5819e4aaf5ff907ff9e22da708527be96ef9de12
Edoardo Holzl <edoardo.holzl@epfl.ch>
2020-12-01 13:41:07 +0100

    Bump version

--- mlbench_core/__init__.py
+++ mlbench_core/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for mlbench_core."""
 
-__version__ = "2.4.0-dev307"
+__version__ = "2.4.0-dev308"
 
 from . import api, controlflow, dataset, evaluation, lr_scheduler, models, optim, utils
