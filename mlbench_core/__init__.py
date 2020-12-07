912fc9458d092bcf3b8eed9177934a29a2c307a5
ehoelzl <ehoelzl@users.noreply.github.com>
2020-12-07 13:59:50 +0000

    Bump version and update Changelog

--- mlbench_core/__init__.py
+++ mlbench_core/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for mlbench_core."""
 
-__version__ = "2.4.0-dev309"
+__version__ = "3.0.0"
 
 from . import api, controlflow, dataset, evaluation, lr_scheduler, models, optim, utils
